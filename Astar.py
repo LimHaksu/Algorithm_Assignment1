b2f93b679a6b3cf242f2c35d1eaa3ae5a57c019d
Neroren <limhaksu@naver.com>
2018-03-18 01:16:57 +0900

    correct A* (increase g by 1 when go to next node)

--- Astar.py
+++ Astar.py
@@ -32,40 +32,44 @@ def make_child_node(current_node, parrent_node, height, width, maze):
     x = current_node.x
     y = current_node.y + 1
     if not(x == parrent_node.x and y == parrent_node.y):
-        if width > x >= 0 and height > y >= 0 and maze[y][x] == 0:
+        if width > x >= 0 and height > y >= 0 and maze[y][x] == 0 and check[y][x] == 0:
             h=width-x+height-y-2
-            current_node.to.append(node(x,y,current_node, current_node.g, h))
-            heapq.heappush(hq, node(x,y,current_node, current_node.g, h))
+            current_node.to.append(node(x,y,current_node, current_node.g+1, h))
+            heapq.heappush(hq, node(x,y,current_node, current_node.g+1, h))
+            check[y][x] = 1
             #make_child_node(current_node.to[i], current_node, h, w, maze)
 
     # the right node of the current node
     x = current_node.x + 1
     y = current_node.y
     if not(x == parrent_node.x and y == parrent_node.y):
-        if width > x >= 0 and height > y >= 0 and maze[y][x] == 0:
+        if width > x >= 0 and height > y >= 0 and maze[y][x] == 0 and check[y][x] == 0:
             h=width-x+height-y-2
-            current_node.to.append(node(x,y,current_node, current_node.g, h))
-            heapq.heappush(hq, node(x,y,current_node, current_node.g, h))
+            current_node.to.append(node(x,y,current_node, current_node.g+1, h))
+            heapq.heappush(hq, node(x,y,current_node, current_node.g+1, h))
+            check[y][x] = 1
             #make_child_node(current_node.to[i], current_node, h, w, maze)
 
     # the left node of the current node
     x = current_node.x - 1
     y = current_node.y
     if not(x == parrent_node.x and y == parrent_node.y):
-        if width > x >= 0 and height > y >= 0 and maze[y][x] == 0:
+        if width > x >= 0 and height > y >= 0 and maze[y][x] == 0 and check[y][x] == 0:
             h=width-x+height-y-2
-            current_node.to.append(node(x,y,current_node, current_node.g, h))
-            heapq.heappush(hq, node(x,y,current_node, current_node.g, h))
+            current_node.to.append(node(x,y,current_node, current_node.g+1, h))
+            heapq.heappush(hq, node(x,y,current_node, current_node.g+1, h))
+            check[y][x] = 1
             #make_child_node(current_node.to[i], current_node, h, w, maze)
 
     # the upper node of the current node
     x = current_node.x
     y = current_node.y - 1
     if not(x == parrent_node.x and y == parrent_node.y):
-        if width > x >= 0 and height > y >= 0 and maze[y][x] == 0:
+        if width > x >= 0 and height > y >= 0 and maze[y][x] == 0 and check[y][x] == 0:
             h=width-x+height-y-2
-            current_node.to.append(node(x,y,current_node, current_node.g, h))
-            heapq.heappush(hq, node(x,y,current_node, current_node.g, h))
+            current_node.to.append(node(x,y,current_node, current_node.g+1, h))
+            heapq.heappush(hq, node(x,y,current_node, current_node.g+1, h))
+            check[y][x] = 1
             #make_child_node(current_node.to[i], current_node, h, w, maze)
     if hq:
         q = heapq.heappop(hq)
@@ -93,6 +97,7 @@ for i in range(0, int(testcases)):
     width = int(line[1]) # with of maze
     hq = []
     # make maze from the input file
+    check = [[0 for col in range(width)] for row in range(height)]
     maze = [[0 for col in range(width)] for row in range(height)]
     for j in range(0, height):
         line = f.readline().split(',', width-1)
