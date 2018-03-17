bab43397fc0f13cdc10b1fe05b6e74c1d66f1614
Neroren <limhaksu@naver.com>
2018-03-18 00:04:46 +0900

    adjust A*, make DFS algorithm

--- DFS.py
+++ DFS.py
@@ -1,4 +1,5 @@
 import heapq
+import pdb
 class node:
     def __init__(self, x, y, from_node, g, h):
         self.from_node = from_node
@@ -21,55 +22,69 @@ class node:
 # 2차원 배열 생성 방법
 # L = [[0 for cols in range(m)]for rows in range(n)]
 
-def make_child_node(current_node, parrent_node, height, width, maze):
+def make_child_node(current_node, parrent_node):
     print(current_node.y,current_node.x)
+    #pdb.set_trace()
     #print explored node
     if(current_node.y == height-1 and current_node.x == width-1):
+        goal_state[0] = True
         print_tree(current_node)
         return 0
-
     # the lower node of the current node
     x = current_node.x
     y = current_node.y + 1
-    if not(x == parrent_node.x and y == parrent_node.y):
-        if width > x >= 0 and height > y >= 0 and maze[y][x] == 0:
+    if not(x == parrent_node.x and y == parrent_node.y) and goal_state[0] == False:
+        if width > x >= 0 and height > y >= 0 and maze[y][x] == 0 and check[y][x] == 0:
             h=width-x+height-y-2
-            current_node.to.append(node(x,y,current_node, current_node.g, h))
-            heapq.heappush(hq, node(x,y,current_node, current_node.g, h))
+            child = node(x,y,current_node, current_node.g, h)
+            current_node.to.append(child)
+            check[y][x] = 1
+            make_child_node(child, current_node)
+            #heapq.heappush(hq, node(x,y,current_node, current_node.g, h))
             #make_child_node(current_node.to[i], current_node, h, w, maze)
 
     # the right node of the current node
     x = current_node.x + 1
     y = current_node.y
-    if not(x == parrent_node.x and y == parrent_node.y):
-        if width > x >= 0 and height > y >= 0 and maze[y][x] == 0:
+    if not(x == parrent_node.x and y == parrent_node.y) and goal_state[0] == False:
+        if width > x >= 0 and height > y >= 0 and maze[y][x] == 0 and check[y][x] == 0:
             h=width-x+height-y-2
-            current_node.to.append(node(x,y,current_node, current_node.g, h))
-            heapq.heappush(hq, node(x,y,current_node, current_node.g, h))
+            child = node(x,y,current_node, current_node.g, h)
+            current_node.to.append(child)
+            check[y][x] = 1
+            make_child_node(child, current_node)
+            #heapq.heappush(hq, node(x,y,current_node, current_node.g, h))
             #make_child_node(current_node.to[i], current_node, h, w, maze)
 
     # the left node of the current node
     x = current_node.x - 1
     y = current_node.y
-    if not(x == parrent_node.x and y == parrent_node.y):
-        if width > x >= 0 and height > y >= 0 and maze[y][x] == 0:
+    if not(x == parrent_node.x and y == parrent_node.y) and goal_state[0] == False:
+        if width > x >= 0 and height > y >= 0 and maze[y][x] == 0 and check[y][x] == 0:
             h=width-x+height-y-2
-            current_node.to.append(node(x,y,current_node, current_node.g, h))
-            heapq.heappush(hq, node(x,y,current_node, current_node.g, h))
+            child = node(x,y,current_node, current_node.g, h)
+            current_node.to.append(child)
+            check[y][x] = 1
+            make_child_node(child, current_node)
+            #heapq.heappush(hq, node(x,y,current_node, current_node.g, h))
             #make_child_node(current_node.to[i], current_node, h, w, maze)
 
     # the upper node of the current node
     x = current_node.x
     y = current_node.y - 1
-    if not(x == parrent_node.x and y == parrent_node.y):
-        if width > x >= 0 and height > y >= 0 and maze[y][x] == 0:
+    if not(x == parrent_node.x and y == parrent_node.y) and goal_state[0] == False:
+        if width > x >= 0 and height > y >= 0 and maze[y][x] == 0 and check[y][x] == 0:
             h=width-x+height-y-2
-            current_node.to.append(node(x,y,current_node, current_node.g, h))
-            heapq.heappush(hq, node(x,y,current_node, current_node.g, h))
+            child = node(x,y,current_node, current_node.g, h)
+            current_node.to.append(child)
+            check[y][x] = 1
+            make_child_node(child, current_node)
+            #heapq.heappush(hq, node(x,y,current_node, current_node.g, h))
             #make_child_node(current_node.to[i], current_node, h, w, maze)
-    if hq:
-        q = heapq.heappop(hq)
-        make_child_node(q, q.from_node, height, width, maze)
+
+    #if hq:
+    #    q = heapq.heappop(hq)
+    #    make_child_node(q, q.from_node, height, width, maze)
 
 def print_tree(leaf_node):
     node = leaf_node
@@ -91,13 +106,15 @@ for i in range(0, int(testcases)):
     #line = line.split(' ', 1 )
     height = int(line[0]) # height of maze
     width = int(line[1]) # with of maze
-    hq = []
+    #hq = []
     # make maze from the input file
+    goal_state = [False]
+    check = [[0 for col in range(width)] for row in range(height)]
     maze = [[0 for col in range(width)] for row in range(height)]
     for j in range(0, height):
         line = f.readline().split(',', width-1)
         for k in range(0, width):
             maze[j][k] = int(line[k])
     start_node = node(0,0,None, 0, width+height-2)
-    make_child_node(start_node, start_node, height, width, maze)
+    make_child_node(start_node, start_node)
 f.close()
